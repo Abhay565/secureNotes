9bea9894bc09b82af341ede3e4e348ab9a11db04
akarshan-mobileFirst <121426671+Abhay565@users.noreply.github.com>
2025-07-28 16:55:20 +0530

    eas for expo bild
    eas for expo bild

--- app.config.ts
+++ app.config.ts
@@ -5,11 +5,14 @@ export default {
     name: 'secureNotes',
     slug: 'secure-notes',
     android: {
-      "package": "com.secureNotes.com"
+      package: 'com.secureNotes.com',
     },
     extra: {
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+      eas: {
+        projectId: '37e35760-f575-4426-aca2-61db8a5e1cb1',
+      },
     },
   },
 };
