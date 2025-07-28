0b7668e6399829d46386d4b30f94f60444ab10ed
akarshan-mobileFirst <121426671+Abhay565@users.noreply.github.com>
2025-07-29 00:04:11 +0530

    Update Login.tsx

--- src/screen/Login.tsx
+++ src/screen/Login.tsx
@@ -15,8 +15,8 @@ import { LinearGradient } from "expo-linear-gradient";
 import { supabase } from "../lib/supabase";
 
 export default function Login({ navigation }) {
-  const [email, setEmail] = useState("abhay12@yopmail");
-  const [password, setPassword] = useState("111111");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
 
   const handleLogin = async () => {
