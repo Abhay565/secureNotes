5af81adbee42aeeb68dd43b5a7123cdd13a6f231
akarshan-mobileFirst <121426671+Abhay565@users.noreply.github.com>
2025-07-28 16:22:18 +0530

    package name added
    package name added

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,9 @@ export default {
   expo: {
     name: 'secureNotes',
     slug: 'secure-notes',
+    android: {
+      "package": "com.secureNotes.com"
+    },
     extra: {
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
